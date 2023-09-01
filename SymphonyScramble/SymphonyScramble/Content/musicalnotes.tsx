67b4510cfbde443a30f6a1e6a460bcc35c7545aa
Nicholas Davidson <nicholasdavidson7@gmail.com>
2023-08-31 23:20:18 -0500

    #3

---
+++ SymphonyScramble/SymphonyScramble/Content/musicalnotes.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="musicalnotes" tilewidth="8" tileheight="8" tilecount="1504" columns="47">
+ <image source="Sprites/Interactables/Notes/musicalnotes.png" trans="f7f7f7" width="383" height="260"/>
+</tileset>
